ecd2bd4d5a424abda88eb21e7a398b29fc3b6b59
imamhossain94 <imamagun94@gmail.com>
2026-04-14 16:50:23 +0600

    feat: initialize app module with Android, Compose, Hilt, and dependency configurations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.newagedevs.gesturevolume"
         minSdk = 26
         targetSdk = 36
-        versionCode = 24
-        versionName = "1.2.4"
+        versionCode = 25
+        versionName = "1.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
